21997e95c892dab97ab408d5b69d36ea8ab195ee
lolcat <tk.orlowski@gmail.com>
2014-10-04 17:15:03 +0200

    Postgres DB Support added.

--- src/main/java/com/pornlen/controller/SmallNumberBean.java
+++ src/main/java/com/pornlen/controller/SmallNumberBean.java
@@ -20,29 +20,29 @@ public class SmallNumberBean {
     @Autowired
     EntityRepository testEntityDao;
 
-	@Max(10)
-	@Min(50)
-   private int smallNumber = 42;
+    @Max(10)
+    @Min(50)
+    private int smallNumber = 42;
 
-   public int getSmallNumber() {
-      return smallNumber;
-   }
+    public int getSmallNumber() {
+        return smallNumber;
+    }
 
-   public void setSmallNumber(int smallNumber) {
-      this.smallNumber = smallNumber;
-   }
+    public void setSmallNumber(int smallNumber) {
+        this.smallNumber = smallNumber;
+    }
 
-   public void showErrors() {
-	   FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "", "The low-priority message is displayed.");
-	   FacesContext.getCurrentInstance().addMessage("smallNumberID", fm);
-	   fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "This error message is oppressed, although it seems to be more important.");
-	   FacesContext.getCurrentInstance().addMessage("smallNumberID", fm);
+    public void showErrors() {
+        FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "", "The low-priority message is displayed.");
+        FacesContext.getCurrentInstance().addMessage("smallNumberID", fm);
+        fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "This error message is oppressed, although it seems to be more important.");
+        FacesContext.getCurrentInstance().addMessage("smallNumberID", fm);
 
-       Entity entity = new Entity();
-       entity.setName("name");
-       testEntityDao.save(entity);
+        Entity entity = new Entity();
+        entity.setName("name");
+        testEntityDao.save(entity);
 
-       System.out.println("--> Error <--");
-   }
+        System.out.println("--> Error <--");
+    }
 
 }
