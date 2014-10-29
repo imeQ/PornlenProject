b892b202820b8b6549e13e28f215eae6bb0aeee5
lolcat <tk.orlowski@gmail.com>
2014-10-29 22:17:21 +0100

    Database migration - initial

--- src/main/resources/db/migration/V1_2__InitialMigration.sql
+++ src/main/resources/db/migration/V1_2__InitialMigration.sql
@@ -1,3 +1,1 @@
-alter table pornlen.driver modify column pin integer;
-alter table pornlen.driver modify column password integer;
-insert into pornlen.application_user(last_modified_date, name) values (now(), 'Mietek');
+insert into pornlen.applicationUser(lastModifiedDate, name) values (now(), 'Mietek');
